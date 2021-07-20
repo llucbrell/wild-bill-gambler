ef6eb6f038bd98f75855060dbfe3f9b236ef2a82
Lucas C/ llucbrell <lucas@groucho.com>
2021-07-20 19:32:00 +0200

    commented refactorized the code in game.js

--- src/game/scenes/game.js
+++ src/game/scenes/game.js
@@ -41,77 +41,85 @@ export class Game extends Phaser.Scene {
     create(){
         let initial_x_pos = 150;
         let initial_y_pos = 120;
-
-       // set the position of the cards 
-        /* this.postionValue = ()=> {
-            let iterations = this.cardValues.length;
-            for(let i = 0; i < iterations; i++){
-                let randomNumberFromArray = Phaser.Math.RND.pick(this.fakeValues);
-                console.log(randomNumberFromArray);
-                let index = this.fakeValues.indexOf(randomNumberFromArray);
-                this.fakeValues.splice(index, 1);
-                console.log(this.fakeValues);
-                this.cardPositions.push(randomNumberFromArray);
-            }
-        }; */
-
-        const getCardName = (value) => {
-	        return `card${value}`
-        }
-
-
+        
+        // make a shuffle
         this.postionValue();
 
-
+        // asociate card images, positions and listeners
         for(let i=0; i<this.cardValues.length; i++){
+            // dynamic name asociation
             let cardname = `card${this.cardPositions[i]}`;
+            // put a second line of cards
             if(i==5) initial_x_pos = 150;
             if(i>4) initial_y_pos = 320;
+            // relate the image, the card and the backcard with same index
             let card=this.add.image(initial_x_pos, initial_y_pos, cardname);
             let backCard = this.add.image(initial_x_pos, initial_y_pos, 'back-card');
             backCard.index=i;
             card.index=i;
+            // move and point to the next postion
             initial_x_pos+=130;
+            // set the scale of the cards (images are too big)
             card.setScale(0.18);
-            card.visible = false;
             backCard.setScale(0.18);
+            // hide the value cards
+            card.visible = false;
+            // set the backcard image interactive (add a listener)
             backCard.setInteractive();
+            // add the cards to arrays to have 2 arrays of cars
             this.backCardArray.push(backCard);
             this.cardArray.push(card);
         }
+        // relate the onclick with the custom handler
         this.input.on('gameobjectdown', this.onClickCard.bind(this));
     }
-onClickCard(pnt, card){
-    console.log(card);
-    this.flipCard(card.index);
-}
-flipCard(index){
-    this.flippedCards+=1;
-    this.backCardArray[index].visible=false;
-    this.cardArray[index].visible=true;
-    this.result += this.cardPositions[index];
-    if(this.flippedCards ==5) this.endGame();
-}
-endGame(){
-score.addToScore(this.result, 'somejsontoken');
-this.scene.pause();
-const sceneEnd = this.scene.get('end');
-sceneEnd.scene.start();
-//this.scene.add("end", new End(this.result));
-}
-initGame(){
-    this.scene.restart();
-}
-postionValue() {
-    let iterations = this.cardValues.length;
-    for(let i = 0; i < iterations; i++){
-        let randomNumberFromArray = Phaser.Math.RND.pick(this.fakeValues);
-        console.log(randomNumberFromArray);
-        let index = this.fakeValues.indexOf(randomNumberFromArray);
-        this.fakeValues.splice(index, 1);
-        console.log(this.fakeValues);
-        this.cardPositions.push(randomNumberFromArray);
+
+    /**
+     * Custom handler for gameobjectdown 
+     * @param {Phaser.Input.Pointer} pnt 
+     * @see https://newdocs.phaser.io/docs/3.55.0/Phaser.Input.Pointer
+     * @param {Phaser.GameObjects.Image} card 
+     * @see https://newdocs.phaser.io/docs/3.54.0/Phaser.GameObjects.Image
+     */
+    onClickCard(pnt, card){
+        this.flipCard(card.index);
     }
+
+    /**
+     * Make invisibl the backcard and visible the value card in base of an array index. 
+     * @param {number} index 
+     */
+    flipCard(index){
+        this.flippedCards+=1;
+        this.backCardArray[index].visible=false;
+        this.cardArray[index].visible=true;
+        this.result += this.cardPositions[index];
+        if(this.flippedCards ==5) this.endGame();
+    }
+
+    /**
+     * Pause the scene 'game' and start 'end' scene
+     */
+    endGame(){
+        score.addToScore(this.result, 'somejsontoken');
+        this.scene.pause();
+        const sceneEnd = this.scene.get('end');
+        sceneEnd.scene.start();
+        //this.scene.add("end", new End(this.result));
+    }
+    /**
+     * Make a shuffle of the values of cars and set them in a another array
+     */
+    postionValue() {
+        let iterations = this.cardValues.length;
+    
+        for(let i = 0; i < iterations; i++){
+            // pick a random number from an array 
+            let randomNumberFromArray = Phaser.Math.RND.pick(this.fakeValues);
+            let index = this.fakeValues.indexOf(randomNumberFromArray);
+            this.fakeValues.splice(index, 1);
+            this.cardPositions.push(randomNumberFromArray);
+        }
 };
 
 }
